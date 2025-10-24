e638f42b293e0debf57fcc69a8b2452bde5b9e63
Pierre Haderer <pierre.haderer@gmail.com>
2025-10-24 15:29:15 +0200

    Add last descriptions and posters + fix bug on service "all movies"

--- services/all_movies.php
+++ services/all_movies.php
@@ -1,5 +1,5 @@
 <?php
-    header('Access-Control-Allow-Origin: *'); // To be able to call this with an ajax call from another site.
+    //header('Access-Control-Allow-Origin: *'); // To be able to call this with an ajax call from another site.
     $file_map_id_title = "map_id_title.txt";
     $file_map_id_card = "map_id_card.txt";
     $posterDirectory = 'posters';
@@ -10,25 +10,29 @@
     $response["message"] = "OK";
     $response["content"] = array();
 
-    $string_map_id_card = file_get_content($file_map_id_card);
+    $string_map_id_card = file_get_contents($file_map_id_card);
     $map_id_card = array();
     foreach (explode("\n", $string_map_id_card) as $id_card) {
         $exploded_id_card = explode(" ", $id_card);
-        $map_id_card[$exploded_id_card[0]] = $exploded_id_card[1];
+        if (sizeof($exploded_id_card) >= 2) {
+            $map_id_card[$exploded_id_card[0]] = $exploded_id_card[1];
+        }
     }
 
-    $string_map_id_title = file_get_content($file_map_id_title);
+    $string_map_id_title = file_get_contents($file_map_id_title);
     foreach (explode("\n", $string_map_id_title) as $id_title) {
         $exploded_id_title = explode(" ", $id_title);
-        $id = $exploded_id_title[0];
-        $title = $exploded_id_title[1];
-        $element = array();
-        $element["id"] = $id;
-        $element["title"] = str_replace("_", " ", $title);
-        $element["card"] = $map_id_card[$id];
-        $element["description"] = $descriptionDirectory . "/" . substr($title, 0, 1) . "/" . $title . ".jpg";
-        $element["poster"] = $posterDirectory . "/" . substr($title, 0, 1) . "/" . $title . ".jpg";
-        array_push($response["content"], $element);
+        if (sizeof($exploded_id_title) >= 2) {
+            $id = $exploded_id_title[0];
+            $title = $exploded_id_title[1];
+            $element = array();
+            $element["id"] = $id;
+            $element["title"] = str_replace("_", " ", $title);
+            $element["card"] = $map_id_card[$id];
+            $element["description"] = $descriptionDirectory . "/" . substr($title, 0, 1) . "/" . $title . ".jpg";
+            $element["poster"] = $posterDirectory . "/" . substr($title, 0, 1) . "/" . $title . ".jpg";
+            array_push($response["content"], $element);
+        }
     }
 
     echo(json_encode($response, JSON_UNESCAPED_SLASHES));
